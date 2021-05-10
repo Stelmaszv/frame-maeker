fef5586e92ce5ddd13dc36c057d3548338952c24
test <MY_NAME@example.com>
2021-05-10 12:29:39 +0200

    Add method "set_menu" in class "Run" (core.py)

--- core.py
+++ core.py
@@ -1,8 +1,24 @@
 class Run:
+
+    dir='rwg'
+
     def start(self):
         run=True
 
         while run != False:
+            self.set_menu()
             action= input('What do you whant to do ? ')
             if action == '0':
                 run = False
+
+            if action == '1':
+                print('dir')
+
+    def set_menu(self):
+        print('0: exit')
+        if len(self.dir)==0:
+            print('1:set dir')
+        else:
+            print('1:start')
+            print('2:change dir')
+            print('3:list dir')
